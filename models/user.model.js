5b39dc82fbe20ef8c83d9fafceafa1c64b27eff0
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-02-11 15:48:21 +0100

    Adjusted model for user

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    login: { type: String, required: true },
-    password: { type: String, required: true },
+    login: { type: String, required: true, minlength: 6, maxlength: 30 },
+    password: { type: String, required: true, minlength: 6 },
     avatar: { type: String, required: true },
-    phoneNumber: { type: Number, required: true },
+    phoneNumber: { type: Number, required: true, minlength: 9, maxlength: 25 },
   });
 
 module.exports = mongoose.model('User', userSchema);
