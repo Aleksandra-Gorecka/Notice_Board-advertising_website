0fe9b8c500e28fcee9f4267f51531ce5d665802c
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-02-16 19:39:05 +0100

    Adjusted Footer and NavBar

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -3,14 +3,20 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () =>{
     return (
-        <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
+        <Navbar bg="dark" data-bs-theme="dark" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
             <Container>
-                <Navbar.Brand>Ads.app</Navbar.Brand>
+                <Navbar.Brand>Notice Board</Navbar.Brand>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="justify-content-end flex-grow-1 pe-3">
                         <Nav.Link to="/" as={NavLink}>
                             Home
                         </Nav.Link>
+                        <Nav.Link as={NavLink} to="/login">
+						    Login
+						</Nav.Link>
+						<Nav.Link as={NavLink} to="/register">
+							Register
+						</Nav.Link>
                         <Nav.Link to="/logout" as={NavLink}>
                             Sign out
                         </Nav.Link>
